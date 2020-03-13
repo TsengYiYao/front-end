67aae01fd093e4e723731bcb623a39c06254b158
TsengYiYao <yiyatsen@gmail.com>
2020-03-13 14:37:47 +0800

    4. Adding a UserItem Component

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import UsersList from './../components/UsersList';
 
 const Users = () => {
-  return <h2>Users Works</h2>
+  const USERS = [{ id: 'u1', name: 'Max Schwarz', image: 'https://images.pexels.com/photos/413959/pexels-photo-413959.jpeg?cs=srgb&dl=beautiful-beige-face-fashion-413959.jpg&fm=jpg', places: 3 }]
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
